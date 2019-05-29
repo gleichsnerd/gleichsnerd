717c148b9fc9ab6ccac3a9338af2d1313c56bbd2
gleichsnerd <gleichsnerd@gmail.com>
2019-05-28 21:40:07 -0400

    Kssssssssshhhhhhhhhhhhhhhhh
    - WE HAVE STATIC NOW YAY
    - Revamped tv-static.js to actually do what it says
    - Whole bunch of work to get threejs as a global import so we can use the non-import friendly examples
    - Webpack config is much nicer now and supports breaking out the vendor package as well as a watching dev server

--- src/tv-static.js
+++ src/tv-static.js
@@ -1,45 +1,115 @@
-import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
 export default function TVStatic(canvasId) {
-    console.log("TV STATIC WARMING UP, CANVAS ID:", canvasId);
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth/window.innerHeight,
-        0.1,
-        1000
-    );
-    const canvas = document.getElementById(canvasId);
-    
-    const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        canvas: canvas
-    });
+    // Fundamentals
+    let scene, camera, canvas, renderer, composer, width, height;
 
-    camera.position.set(5, 5, 0);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    // Passes
+    let renderPass, filmPass, staticPass, copyPass;
 
-    renderer.setSize( window.innerWidth, window.innerHeight);
-    renderer.setClearColor( 0xeeeeee);
+    // Time management
+    let lastAnimateTime = Date.now().valueOf();
 
-    let plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(5, 5, 5, 5),
-        new THREE.MeshBasicMaterial({
-            color: 0x222222,
-            wireframe: true
-        })
-    );
-    plane.rotateX(Math.PI / 2);
-    scene.add(plane);
+    function init() {
+        width = window.innerWidth;
+        height = window.innerHeight;
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.addEventListener("change", function(){ renderer.render(scene, camera); });
+        /** Setup the basics **/
+        scene = new THREE.Scene();
 
-    renderer.render(scene, camera);
+        // Since we're just projecting onto a plane, we can treat this as 2D to make it simple
+        camera = new THREE.OrthographicCamera(
+            60,             // FOV
+            width / height, // Aspect ratio
+            0,              // Near clip
+            1,              // Far clip
+        );
+        scene.add(camera);
 
-    
+        // Wiring up to our canvas element and setting up renderer
+        canvas = document.getElementById(canvasId);
+        renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            canvas: canvas
+        });
+        renderer.setSize(width, height);
+        renderer.setClearColor(0x0a0a0a);
 
+        /** Setup composer and passes */
+        initComposer();
+        initPasses();
 
+        /** Handle external events **/
+        window.addEventListener('resize', onWindowResize, false);
+    }
+
+    function initComposer() {
+        composer = new THREE.EffectComposer(renderer);
+    }
+
+    function initPasses() {
+        if(composer == null) {
+            console.error("No composer initialized; you need to call initComposer before the first setup of your passes");
+        }
+
+        /** Setting up passes **/
+        renderPass = new THREE.RenderPass(scene, camera);
+
+        staticPass = new THREE.ShaderPass(THREE.StaticShader);
+        staticPass.uniforms["amount"].value = 0.5;
+        staticPass.uniforms["size"].value = 1;
+        staticPass.uniforms["time"].value = 0.0;
+
+        filmPass = new THREE.FilmPass(
+            0.3,        // noise intensity
+            1,          // scanline intensity
+            1000,       // scanline count
+            false,      // grayscale
+        );
+        filmPass.uniforms["time"].value = 0.0;
+
+        copyPass = new THREE.ShaderPass(THREE.CopyShader);
+        copyPass.renderToScreen = true;
+
+        composer.addPass(renderPass);
+        composer.addPass(staticPass);
+        composer.addPass(filmPass);
+        composer.addPass(copyPass);
+    }
+
+    function channelSwitch() {
+        // TODO - Move this to its own file which will mount to another canvas
+        const planeGeometry = new THREE.PlaneGeometry(width, 1);
+        const planeMaterial = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        });
+        const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+        scene.add(planeMesh)
+    }
+
+    function onWindowResize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+
+        initComposer();
+        initPasses();
+    }
+
+    function animate() {
+        let now = Date.now().valueOf();
+        let delta = (now - lastAnimateTime) / 1000;
+        lastAnimateTime = now;
+
+        filmPass.uniforms["time"].value += delta;
+        staticPass.uniforms["time"].value += delta;
+
+        requestAnimationFrame(animate);
+        composer.render(delta);
+    }
+
+    init();
+    animate();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,49 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/gleichsnerd.js',
+    entry: {
+        "three-controls": [
+            path.resolve(__dirname, "node_modules/three/examples/js/controls/OrbitControls.js"),
+        ],
+        "three-postprocessing": [
+            path.resolve(__dirname, "node_modules/three/examples/js/postprocessing/EffectComposer.js"),
+            path.resolve(__dirname, "node_modules/three/examples/js/postprocessing/ShaderPass.js"),
+            path.resolve(__dirname, "node_modules/three/examples/js/postprocessing/RenderPass.js"),
+            path.resolve(__dirname, "node_modules/three/examples/js/postprocessing/FilmPass.js"),
+        ],
+        "three-shaders": [
+            path.resolve(__dirname, "node_modules/three/examples/js/shaders/CopyShader.js"),
+            path.resolve(__dirname, "node_modules/three/examples/js/shaders/FilmShader.js"),
+            path.resolve(__dirname, "deps/StaticShader.js"),
+        ],
+        "bundle": "./src/gleichsnerd.js",
+    },
     mode: "development",
-    devtool: "eval-source-map",
+    devtool: "source-map",
     devServer: {
-        contentBase: './dist',
+        contentBase: "./dist",
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: "./node_modules/three/build/three.min.js",
+                to: ""
+            }
+        ])
+    ],
+    optimization: {
+        splitChunks: {
+            chunks (chunk) {
+                return chunk.name === "bundle";
+            },
+            name () {
+                return "vendors";
+            }
+        }
     },
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist")
     }
 };
