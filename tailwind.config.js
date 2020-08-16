b793b12961e4c99405ea640abe939217e0e2a938
Adam Gleichsner <gleichsnerd@gmail.com>
2020-08-16 00:50:35 -0400

    ADDED PORTFOLIO STARTER YAY

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,18 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#5BC0BE'
+        primary: '#5BC0BE',
+        'primary-light': '#5BC0BE47'
       },
       fontFamily: {
         'sans': ['"Space Grotesk"']
+      },
+      maxHeight: {
+        'card': '14rem'
+      },
+      boxShadow: {
+        // default: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        default: '0 10px 15px -3px theme("colors.primary-light"), 0 4px 6px -2px theme("colors.primary-light")',
       }
     },
   },
